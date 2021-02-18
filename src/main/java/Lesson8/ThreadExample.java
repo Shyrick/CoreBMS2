ee9c0b2271bed63ec586b8e6fc52359246430ef8
technical.team <Lykun@mail.ru>
2021-02-18 18:08:53 +0300

    L8 from Work

---
+++ src/main/java/Lesson8/ThreadExample.java
@@ -0,0 +1,17 @@
+package Lesson8;
+
+public class ThreadExample {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {    // Runnable - интерфейс, который требует переопределения метода run()
+            @Override                           // и сделать это можно здесь (при создании экземпляра),
+            public void run() {                 // а можно и создав отдельный класс, который будетимплементировать
+         // здесь будет код, который будет код  // интерфейс Runnable и уже в этом классе переопределить run()
+         // который будет выполняться
+         // в потоке
+                System.out.println("Hello from new thread");
+            }
+        };
+    }
+}
