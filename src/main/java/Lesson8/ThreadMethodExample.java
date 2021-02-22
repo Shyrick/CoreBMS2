b274c9d9f754c1e4b82750ca27bc81cc9604fca7
technical.team <Lykun@mail.ru>
2021-02-22 14:51:42 +0300

    L8 from Work_3

--- src/main/java/Lesson8/ThreadMethodExample.java
+++ src/main/java/Lesson8/ThreadMethodExample.java
@@ -1,5 +1,71 @@
 package Lesson8;
 
-public class ThreadMethodExample {
-    1:27:00
+public class ThreadMethodExample { 1:42:00
+
+    static Thread showTimer2 (int timer){
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int secconds = 0;
+                while (secconds < timer){
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(Thread.currentThread().getName() + " " +  ++secconds);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+
+
+                }
+                }
+            });
+            thread.start();
+        return thread;
+    }
+
+
+
+//    static void showTimer (int timer){  // пример метода, в котором создается поток и в который
+//        new Thread(new Runnable() {     // можно что-либо передавать (здесь переменная timer)
+//            @Override
+//            public void run() {
+//                int secconds = 0;
+//                while (secconds < timer){
+//                    try {
+//                        Thread.sleep(1000);
+//                        System.out.println(Thread.currentThread().getName() + " " +  ++secconds);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                        break;
+//                    }
+//
+//
+//                }
+//            }
+//        }).start();
+//    }
+
+//    static int i =0;
+    public static void main(String[] args) throws InterruptedException {
+
+        showTimer2(3).join();
+        showTimer2(4).join();
+        showTimer2(5).join();
+
+//        showTimer(3);
+//        showTimer(4);
+//        showTimer(5);
+//------------------------------------
+//        for (i = 0; i < 5; i++) {
+//             new Thread(new Runnable() {
+//                 @Override
+//                 public void run() {
+//                     System.out.println("i = " + i); //Вывод будет хаотичным, причем может даже выводить i=5 несколько
+//                                                    // раз, т.к. потоки создаются долго, а for работает быстро
+//                     // и пока будут создаваться потоки i уже может быть равно 5 (значение при выходе из цикла)
+//                 }
+//             }).start();
+//        }
+    }
 }
