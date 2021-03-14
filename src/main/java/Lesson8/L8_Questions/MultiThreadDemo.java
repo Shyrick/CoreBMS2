a3f4c06cd2c10bde60dacf76bf0f86c3fac773e6
technical.team <Lykun@mail.ru>
2021-03-14 19:04:45 +0300

    Q8 from Work_1

--- src/main/java/Lesson8/L8_Questions/MultiThreadDemo.java
+++ src/main/java/Lesson8/L8_Questions/MultiThreadDemo.java
@@ -1,8 +1,27 @@
 package Lesson8.L8_Questions;
 
+import java.util.Scanner;
+
 public class MultiThreadDemo {
+
+    32^00
+
+    static long fibonacci (int x){
+        if (x <- 1) return 1;
+        else return fibonacci(x-1)+ fibonacci(x-2);
+    }
+
     public static void main(String[] args) {
-        23^00
+        Scanner scaner = new Scanner(System.in);
+
+        int x = scaner.nextInt();
+        long startTime = System.currentTimeMillis();
+        long f = fibonacci(x);
+        long endtime = System.currentTimeMillis();
+
+        long duration = endtime - startTime;
+        System.out.println("Fibonacci (" + x + ") = " + f + "\t\t duration = " + duration );
+
     }
 
 }
