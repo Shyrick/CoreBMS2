d203bf59ac2e3bce572acd214b43bcf534becb63
Shyrick <lykun@mail.ru>
2021-03-17 22:52:20 +0300

    From home Q8_3

--- src/main/java/Lesson8/L8_Questions/MultiThreadDemo.java
+++ src/main/java/Lesson8/L8_Questions/MultiThreadDemo.java
@@ -3,24 +3,50 @@ package Lesson8.L8_Questions;
 import java.util.Scanner;
 
 public class MultiThreadDemo {
+    39:00
 
-    32^00
+    public static void MultiFibonacci (int n, int countOfTreads) throws InterruptedException {
+      //  int cores = Runtime.getRuntime().availableProcessors();  - возвращает количество ядер системы
 
-    static long fibonacci (int x){
+        Thread [] threads = new Thread [countOfTreads];
+        for (int i = 0; i < countOfTreads ; i++) {
+            threads [i] = new Thread(() -> {
+                fibonacci (n);
+            });
+            threads [i].start();
+        }
+
+        for (int i = 0; i < countOfTreads ; i++) {
+            threads [i].join();
+
+        }
+
+
+    }
+
+
+    public static long fibonacci (int x){
         if (x <- 1) return 1;
         else return fibonacci(x-1)+ fibonacci(x-2);
     }
 
-    public static void main(String[] args) {
-        Scanner scaner = new Scanner(System.in);
-
-        int x = scaner.nextInt();
+    public static void main(String[] args) throws InterruptedException {
         long startTime = System.currentTimeMillis();
-        long f = fibonacci(x);
-        long endtime = System.currentTimeMillis();
+        MultiFibonacci (37, 5);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("duration = " + duration );
 
-        long duration = endtime - startTime;
-        System.out.println("Fibonacci (" + x + ") = " + f + "\t\t duration = " + duration );
+
+//        Scanner scaner = new Scanner(System.in);
+//        int x = scaner.nextInt();
+
+//        long startTime = System.currentTimeMillis();
+//        long f = fibonacci(x);
+//        long endTime = System.currentTimeMillis();
+//
+//        long duration = endTime - startTime;
+//        System.out.println("Fibonacci (" + x + ") = " + f + "\t\t duration = " + duration );
 
     }
 
